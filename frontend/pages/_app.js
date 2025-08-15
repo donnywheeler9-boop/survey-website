ff7fe21bf51a56bab30df416a319543c5d17f44f
donnywheeler9-boop <donnywheeler9@gmail.com>
2025-08-15 17:56:24 +0600

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,1 +1,7 @@
+// frontend/pages/_app.js
 
+import '../styles/globals.css';  // Add this line to import the globals.css file
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
